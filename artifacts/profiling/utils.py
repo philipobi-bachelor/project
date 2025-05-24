e391641b41e92640584bbee71272541ef48e1c62
philipobi <philipobiu@gmail.com>
2025-05-25 00:34:12 +0200

    Trying out arango db

---
+++ artifacts/profiling/utils.py
@@ -0,0 +1,19 @@
+import os
+from pprint import pprint
+
+def readlines_reversed(path, bufsiz=8192):
+    # based on https://stackoverflow.com/a/23646049
+    with open(path, "rb") as f:
+        pos = f.seek(0, os.SEEK_END)
+        segment = None
+        while pos > 0:
+            pos_new = f.seek(max(pos - bufsiz, 0))
+            buf = f.read(pos - pos_new)
+            lines = buf.split("\n".encode())
+            if segment is not None:
+                lines[-1] += segment
+            segment = lines[0]
+            yield from map(lambda l: l.decode(), reversed(lines[1:]))
+            pos = pos_new
+        if segment is not None:
+            yield segment.decode()
