c33bc1f2cc6a20f57df1fafff649c27f4d3404a4
philipobi <philipobiu@gmail.com>
2025-07-20 05:57:49 +0200

    Add support to render the table as latex

--- artifacts/profiling/utils.py
+++ artifacts/profiling/utils.py
@@ -1,4 +1,80 @@
 import os
+from typing import Iterable, Iterator
+
+
+class Col:
+    borderLeft = 0b10
+    borderRight = 0b01
+
+    def __init__(
+        self,
+        name: str,
+        rows: Iterable[str],
+        colType: str,
+        borders: int | None = None,
+    ):
+        self.name = name
+        self.rows = iter(rows)
+        self.colType = colType
+        self.borders = borders if borders is not None else 0b00
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.rows)
+
+
+class Table:
+    defs = []
+
+    def __init__(
+        self,
+        *cols: Col,
+        rowLines: bool = False,
+        colLines: bool = False,
+    ):
+        self.cols = cols
+        self.rowLines = rowLines
+        self.colLines = colLines
+
+    def lines(self) -> Iterator[str]:
+        preamble = ""
+        if not self.colLines:
+            for colLeft, colRight in zip(self.cols[:-1], self.cols[1:]):
+                sep = (
+                    " | "
+                    if (
+                        colLeft.borders & Col.borderRight
+                        | colRight.borders & Col.borderLeft
+                    )
+                    else " "
+                )
+                preamble += sep + colRight.colType
+            firstCol = self.cols[0]
+            lastCol = self.cols[-1]
+            preamble = (
+                ("| " if firstCol.borders & Col.borderLeft else "")
+                + firstCol.colType
+                + preamble
+                + ("|" if lastCol.borders & Col.borderRight else "")
+            )
+        else:
+            preamble = "| " + " | ".join((col.colType for col in self.cols)) + " |"
+
+        linesep = r" \\\hline" if self.rowLines else r" \\"
+        yield "{"
+        yield from Table.defs
+        yield r"\begin{tabular}{" + preamble + "}"
+        yield " & ".join((col.name for col in self.cols)) + linesep
+        for rowCells in zip(*self.cols):
+            yield " & ".join(rowCells) + linesep
+        yield r"\end{tabular}"
+        yield "}"
+
+    def render(self) -> str:
+        return "\n".join(self.lines())
+
 
 def readlinesReversed(path, bufsiz=8192):
     # based on https://stackoverflow.com/a/23646049
