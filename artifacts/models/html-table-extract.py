3ecc2fe0c037711f6d9ad28ed4b7c5c2c5fa8f9b
philipobi <philipobiu@gmail.com>
2025-08-05 23:35:11 +0200

    Replace livebench with artificial-analysis

--- artifacts/models/html-table-extract.py
+++ artifacts/models/html-table-extract.py
@@ -1,18 +1,6 @@
 import pandas as pd
 
 if __name__ == "__main__":
-
-    [df] = pd.read_html("livebench.html")
-
-    df.sort_values(
-        "Coding Average",
-        ascending=False,
-    ).to_csv(
-        "livebench-coding.csv",
-        index=False,
-        float_format="%.2f",
-    )
-
     [df] = pd.read_html("lmarena.html")
 
     df.sort_values("Coding").to_csv(
